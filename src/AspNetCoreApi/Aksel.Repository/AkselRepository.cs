3ceefe1e86729dcc9c87eba58b71c9019ddce619
Aksel Arzuman <akselarzuman@gmail.com>
2019-05-11 17:12:14 +0300

    get method implemented

--- src/AspNetCoreApi/Aksel.Repository/AkselRepository.cs
+++ src/AspNetCoreApi/Aksel.Repository/AkselRepository.cs
@@ -1,14 +1,23 @@
 using System.Threading.Tasks;
 using Aksel.Models.Entities;
+using Aksel.Repository.Context;
 using Aksel.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Aksel.Repository
 {
     public class AkselRepository : IAkselRepository
     {
+        private readonly AkselDbContext _context;
+
+        public AkselRepository(AkselDbContext context)
+        {
+            _context = context;
+        }
         public async Task<AkselEntity> GetAsync()
         {
-            throw new System.NotImplementedException();
+            AkselEntity entity = await _context.Aksel.FirstOrDefaultAsync();
+            return entity;
         }
     }
 }
