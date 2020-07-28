42fdc8ddbacdf99c0b94e7170e0b1256ac17b23f
Aksel Arzuman <akselarzuman@gmail.com>
2020-07-29 00:44:49 +0300

    * generic model validators added

---
+++ src/AspNetCoreAuthentication/src/Models/Aksel.ModelValidators/FluentValidator/IModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Aksel.ModelValidators.FluentValidator
+{
+    public interface IModelValidator
+    {
+        public interface IModelValidator
+        {
+            Task ValidateAsync<T>(T model);
+        }
+    }
+}
